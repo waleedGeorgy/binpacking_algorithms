e1657a0afb46fa68775459589bffeafef3574777
Waleed Georgy <49490582+waleedGeorgy@users.noreply.github.com>
2022-12-11 19:50:13 +0300

    final changes
    for now

--- binpacker.py
+++ binpacker.py
@@ -16,16 +16,20 @@ import time
 # Weight and Size are used interchangeably here and elsewhere.
 
 items = []
+items_weights = []
+bins_capacity = []
 
 maxBins = int(input('Insert number of bins: '))
 binCapacity = 0
 for i in range(0, maxBins):
     binCapacity = int(input(f'Insert capacity of bin {i + 1}: '))
+    bins_capacity.append(binCapacity)
 
 item_number = int(input('Inter number of items: '))
 for i in range(0, item_number):
     input_elements = (input(f'Input item {i + 1} name: '), int(input(f'Input item {i + 1} size: ')))
     items.append(input_elements)
+    items_weights.append(input_elements[1])
 
 itemCount = len(items)
 
@@ -43,17 +47,15 @@ problem = LpProblem("The Bin Packing Problem", LpMinimize)
 # Add the objective function.
 problem += lpSum([y[i] for i in range(maxBins)]), "Objective: Minimize the number of bins used"
 
-#
 # This is the constraints section.
-#
 
 # First constraint: For every item, the sum of bins in which it appears must be 1
 for j in items:
     problem += lpSum([x[(j[0], i)] for i in range(maxBins)]) == 1, ("Item can only be in 1 bin - " + str(j[0]))
 
 # Second constraint: For every bin, the number of items in the bin cannot exceed the bin capacity
-for i in range(maxBins):
-    problem += lpSum([items[j][1] * x[(items[j][0], i)] for j in range(itemCount)]) <= binCapacity * y[i], (
+for i in range(len(bins_capacity)):
+    problem += lpSum([items[j][1] * x[(items[j][0], i)] for j in range(itemCount)]) <= bins_capacity[i] * y[i], (
             "Sum of item weights or sizes must be lower than the bin capacity - " + str(i))
 
 # Write the model to disk
@@ -79,4 +81,4 @@ for itemBinPair in x.keys():
             bins[binNum] = [itemNum]
 
 for b in bins.keys():
-    print(str(b) + ": " + str(bins[b]))
+    print(str(b + 1) + ": " + str(bins[b]))
